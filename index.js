8a5f6d55310e2b62992615c91ad4e3da4555e4e9
NEO <NEO@Neos-Air.lan>
2015-12-07 01:27:34 -0500

    Add process.env.PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/static/index.html');
