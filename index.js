cfc035b51b6e89a821ba95e5aeb9508989f5d9f7
NEO <NEO@Neos-Air.lan>
2015-12-07 00:49:05 -0500

    Add function: changing color by tilting

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 server.listen(3000);
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/static/index.html');
 });
+
+app.use('/static', express.static('static'));
 
 io.on('connection', function (socket) {
 	// console.log('a user connected');
 	// socket.on('disconnect', function () {
 	// 	console.log('user disconnected');
 	// })
-	socket.on('device', function (data) {
-		console.log("Device:", data);
+	socket.on('tilt', function (data) {
+		socket.broadcast.emit('tilt', data);
 	});
 })
---
+++ static/socket.js
@@ -0,0 +1,44 @@
+var socket = io();
+var control = document.querySelector('.half:first-child');
+var monitor = document.querySelector('.half:last-child');
+control.addEventListener('click', device);
+monitor.addEventListener('click', device);
+function device (e) {
+	switch (e.currentTarget) {
+		case(control):
+			controlHandler();
+			var theOther = monitor;
+			break;
+		case(monitor):
+			monitorHandler(e.currentTarget);
+			var theOther = control;
+			break;
+	}
+	document.querySelectorAll('.text')[0].style.display = 'none';
+	document.querySelectorAll('.text')[1].style.display = 'none';
+	if(theOther.offsetWidth == document.body.offsetWidth) {
+		theOther.style.height = 0;
+	} else {
+		theOther.style.width = 0;
+	}
+	e.currentTarget.removeEventListener('click', device);
+}
+function controlHandler () {
+	window.addEventListener('deviceorientation', function (e) {
+		var data = {x: e.beta, y: e.gamma, z: e.alpha};
+		socket.emit('tilt', data);
+	});
+}
+function monitorHandler (target) {
+	socket.on('tilt', function (data) {
+		var x = data.x;
+		var y = data.y;
+		var z = data.z > 180 ? data.z - 360 : data.z;
+		var rgb = 'rgb(' + math(x) + ', ' + math(y) + ', ' + math(z) + ')';
+		target.style.background = rgb;
+		console.log(rgb);
+	});
+	function math (n) {
+		return Math.max(Math.min(Math.round((n * 2 + 180) * (255 / 360)), 255), 0);
+	}
+}
